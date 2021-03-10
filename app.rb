b4258682606c98f7a6acb39b2a03ced3c068fc16
Allison _ <allimacq@gmail.com>
2021-03-10 20:34:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    @text_input = PigLatinizer.new(params[:string])
+    
+    erb :translation
+  end
 end
